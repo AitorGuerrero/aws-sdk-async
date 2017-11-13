ed563ce549cd224d3cfaffbaa39a4f3f0d28face
Aitor Guerrero <aguerrerolasarte@gmail.com>
2017-11-13 17:27:57 +0100

    s3 upload

--- src/s3-async.ts
+++ src/s3-async.ts
@@ -2,6 +2,7 @@ import {S3} from 'aws-sdk';
 
 export interface IS3Async {
 	putObject(i: S3.PutObjectRequest): Promise<S3.PutObjectOutput>;
+	upload(i: S3.PutObjectRequest, options?: S3.ManagedUpload.ManagedUploadOptions): Promise<S3.ManagedUpload.SendData>;
 }
 
 export class S3Async implements IS3Async {
@@ -11,6 +12,12 @@ export class S3Async implements IS3Async {
 	public async putObject(i: S3.PutObjectRequest) {
 		return new Promise<S3.PutObjectOutput>(
 			(rs, rj) => this.s3.putObject(i, (err, r) => err ? rj(err) : rs(r)),
+		);
+	}
+
+	public async upload(i: S3.PutObjectRequest, options?: S3.ManagedUpload.ManagedUploadOptions) {
+		return new Promise<S3.ManagedUpload.SendData>(
+			(rs, rj) => this.s3.upload(i, (err, r) => err ? rj(err) : rs(r)),
 		);
 	}
 }
